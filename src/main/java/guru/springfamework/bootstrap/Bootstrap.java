f8a167879155f4335ca3f12a63e39758ebe1c9fb
blipsman <b_lipsman@yahoo.com>
2020-01-05 21:30:46 -0500

    created CategoryService and its implementation. Also added CategoryServiceTest. used the category-service branch

--- src/main/java/guru/springfamework/bootstrap/Bootstrap.java
+++ src/main/java/guru/springfamework/bootstrap/Bootstrap.java
@@ -11,12 +11,11 @@ import org.springframework.stereotype.Component;
 @Component
 public class Bootstrap implements CommandLineRunner{
 
-    private CategoryRepository categoryRepository;
+    private CategoryRepository categoryRespository;
 
-    public Bootstrap(CategoryRepository categoryRepository) {
-        this.categoryRepository = categoryRepository;
+    public Bootstrap(CategoryRepository categoryRespository) {
+        this.categoryRespository = categoryRespository;
     }
-
 
     @Override
     public void run(String... args) throws Exception {
@@ -35,14 +34,14 @@ public class Bootstrap implements CommandLineRunner{
         Category nuts = new Category();
         nuts.setName("Nuts");
 
-        categoryRepository.save(fruits);
-        categoryRepository.save(dried);
-        categoryRepository.save(fresh);
-        categoryRepository.save(exotic);
-        categoryRepository.save(nuts);
+        categoryRespository.save(fruits);
+        categoryRespository.save(dried);
+        categoryRespository.save(fresh);
+        categoryRespository.save(exotic);
+        categoryRespository.save(nuts);
 
 
-        System.out.println("Data Loaded = " + categoryRepository.count() );
+        System.out.println("Data Loaded = " + categoryRespository.count() );
 
     }
 }
